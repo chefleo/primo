1cdd012ff75bb225576d36a2afc9ea4419a30d3d
Leonardo Simone Digiorgio <simon.digiorgio@gmail.com>
2017-09-13 21:25:04 +0200

    Fixed variables

--- Fibonacci.js
+++ Fibonacci.js
@@ -1,11 +1,11 @@
-var numero = 1;
-var numero_2 = 2;
-var finale;
-var passi = 5;
+let numero = 1;
+let numero_2 = 2;
+let finale;
+let passi = 5;
 
 function Fibonacci(numero,numero_2) {
-	for(var i=1;i<=passi;i++){
-		finale = numero+numero_2;
+	for(var i = 1;i <= passi;i++) {
+		finale = numero + numero_2;
 		numero = numero_2;
 		numero_2 = finale;
 	}
