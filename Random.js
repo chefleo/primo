9a7fccf1a927c66141eeac7849de46801916176f
Leonardo Simone Digiorgio <simon.digiorgio@gmail.com>
2017-09-19 14:34:32 +0200

    Delete and united in the file util.js

--- Random.js
+++
@@ -1,7 +0,0 @@
-function getRandomNumber(max,min) {
-	return Math.floor(Math.random()*(max-min+1))+1;
-}
-
-const elem = function getRandomElement(arr) {
-	return arr[Math.floor(Math.random()*arr.length)];
-}
